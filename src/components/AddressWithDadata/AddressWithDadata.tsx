48cb89879188ee866051055f08ee11ce02e82499
ArtemANTIPIN <folureen@gmail.com>
2023-06-10 09:56:29 +0300

    init

---
+++ src/components/AddressWithDadata/AddressWithDadata.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {
+    label: string;
+}
+const AddressWithDadata: React.FC<Props> = () => {
+    return <input placeholder='test' />;
+};
+export default AddressWithDadata;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as AddressWithDadata } from './AddressWithDadata'
